c1a05007ad5c1e85f8ad05b5ef65a5cf022586da
smallpp420 <78730004+SmallPP420@users.noreply.github.com>
2021-11-15 21:03:44 +0000

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,20 +1,75 @@
-#
-# Copyright (C) 2020 The Android Open Source Project
-# Copyright (C) 2020 The TWRP Open Source Project
-# Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+FDEVICE="WSP_sprout"
 
-add_lunch_combo omni_WSP_sprout-userdebug
-add_lunch_combo omni_WSP_sprout-eng
+fox_get_target_device() {
+local chkdev=$(echo "$BASH_SOURCE" | grep $FDEVICE)
+   if [ -n "$chkdev" ]; then
+      FOX_BUILD_DEVICE="$FDEVICE"
+   else
+      chkdev=$(set | grep BASH_ARGV | grep $FDEVICE)
+      [ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
+   fi
+}
+
+if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
+   fox_get_target_device
+fi
+
+if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
+        export PLATFORM_VERSION="16.1.0"
+   	export PLATFORM_SECURITY_PATCH="2099-12-31"
+   	export TW_DEFAULT_LANGUAGE="en"
+	export OF_KEEP_FORCED_ENCRYPTION=0
+	export OF_PATCH_AVB20=1
+	#Screen Settings Tagged Because It Wont Scale Properly
+	#export OF_SCREEN_H=1520
+	export OF_USE_MAGISKBOOT=1
+	export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
+	export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
+	export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
+	export OF_NO_MIUI_PATCH_WARNING=1
+	export OF_USE_GREEN_LED=0
+
+	# use magisk 21.4 for the magisk addon
+	export FOX_USE_SPECIFIC_MAGISK_ZIP=~/Magisk/Magisk-21.4.zip
+  
+  export OF_DISABLE_MIUI_SPECIFIC_FEATURES=1
+	export FOX_USE_BASH_SHELL=1
+	export FOX_ASH_IS_BASH=1
+	export FOX_USE_NANO_EDITOR=1
+	export FOX_USE_TAR_BINARY=1
+	export FOX_USE_ZIP_BINARY=1
+	export FOX_USE_SED_BINARY=1
+	export FOX_USE_XZ_UTILS=1
+	export FOX_REPLACE_BUSYBOX_PS=1
+	export OF_USE_NEW_MAGISKBOOT=1
+	export OF_SKIP_MULTIUSER_FOLDERS_BACKUP=1
+   	export FOX_BUGGED_AOSP_ARB_WORKAROUND="1510672800"; # Tue Nov 14 15:20:00 GMT 2017
+
+        # use system (ROM) fingerprint where available
+        #export OF_USE_SYSTEM_FINGERPRINT=1
+
+	# OTA for custom ROMs
+        #export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
+        #export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
+
+        # -- add settings for R11 --
+        export FOX_R11=1
+        export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
+        # -- end R11 settings --
+
+	# run a process after formatting data to work-around MTP issues
+	export OF_RUN_POST_FORMAT_PROCESS=1
+
+	# let's see what are our build VARs
+	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
+  	   export | grep "FOX" >> $FOX_BUILD_LOG_FILE
+  	   export | grep "OF_" >> $FOX_BUILD_LOG_FILE
+   	   export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
+  	   export | grep "TW_" >> $FOX_BUILD_LOG_FILE
+ 	fi
+
+  	for var in eng user userdebug; do
+  		add_lunch_combo omni_"$FDEVICE"-$var
+  	done
+fi
+#
