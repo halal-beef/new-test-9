0be060da64bc1ae83cf4fcf31567f01a8c9eb5b7
smallpp420 <78730004+SmallPP420@users.noreply.github.com>
2021-11-18 16:38:39 +0000

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -33,6 +33,7 @@ BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 # buildvariant=user
 BOARD_KERNEL_BASE := 0x40078000                                                
 BOARD_KERNEL_PAGESIZE := 2048                                                  
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x11a88000 --tags_offset 0x07808000   # --ramdisk_offset 0x03f88000 --tags_offset 0x0df88000
+BOARD_KERNEL_IMAGE_NAME := zImage
 
 TARGET_SYSTEM_PROP := $(LOCAL_PATH)/system.prop
 TARGET_RECOVERY_INITRC := $(LOCAL_PATH)/recovery/root/init.recovery.mt6761.rc
