2b785f52701da31258e52c2321e402938c6e3bb9
smallpp420 <78730004+SmallPP420@users.noreply.github.com>
2021-11-20 14:35:30 +0000

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -134,7 +134,7 @@ TARGET_RECOVERY_PIXEL_FORMAT := "RGBA_8888"
 TW_SECONDARY_BRIGHTNESS_PATH := /sys/devices/platform/leds-mt65xx/leds/lcd-backlight/brightness
 TW_DEFAULT_BRIGHTNESS := 80
 
-TW_DEVICE_VERSION := - Test Build By areallydumbperson
+TW_DEVICE_VERSION :=  Test Build By areallydumbperson
 #
 #
 #The End!
